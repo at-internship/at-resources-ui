a95e39a89119d8c7abd96816e8b0e99fbba9a7c5
Erick Barreto <erick.barreto@agilethought.com>
2020-10-28 15:38:19 -0600

    it was add funtuion render list story, routing un the file admin and view message

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,11 +2,30 @@ const adminCtrl = {};
 
 // MICROSERVICE - HEROKU - SSO
 //const ssoServiceAPI = require("../services/at-sso-api.service");
+const resourceServiceApi = require("../services/at-resource-api.service");
 
 // AT-RESOURCES - Admin - Index
 adminCtrl.renderIndex = async(req, res) => {
     console.log("--> adminCtrl.renderIndex");
     res.render("admin/index");
+};
+
+// AT-RESOURCES - Admin - Story list
+// adminCtrl.renderStoryList = async (req, res) => {
+//     try{
+//         const responseStoryList = await resourceServiceApi.getStoryList();
+//         console.log("--> adminCtrl.renderStoryList");
+//         const storys = responseStoryList.data;
+//         res.render("/admin/story/list-story", { storys });
+//     } catch {
+//         console.error(err.message);
+//         res.render("/admin/story/list-story", { storys });
+//     }
+// };
+
+adminCtrl.renderStoryList = async (req, res) => {
+    res.render("/admin/story/list-story");
+    console.log("story")
 };
 
 module.exports = adminCtrl;
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 // Admin Controller
-const { renderIndex } = require("../controllers/admin.controller");
+const { renderIndex, renderStoryList } = require("../controllers/admin.controller");
 
 // Helpers
 // const { isAdmin } = require("../helpers/auth");
@@ -12,5 +12,9 @@ const { renderIndex } = require("../controllers/admin.controller");
 
 // AT-RESOURCES - Admin - Index
 router.get("/", renderIndex);
+
+// AT-RESOURCES - Admin - Story list
+router.get("/story", renderStoryList);
+
 
 module.exports = router;

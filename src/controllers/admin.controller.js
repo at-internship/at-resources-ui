be80492994a888d91ccb225df85e0b8ffa34c9df
LuisAT07 <luis.gomez@agilethought.com>
2020-11-03 09:36:12 -0600

    Added admin controllers and admin routes for stories

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -8,5 +8,63 @@ adminCtrl.renderIndex = async(req, res) => {
     console.log("--> adminCtrl.renderIndex");
     res.render("admin/index");
 };
+// AT-RESOURCES - Admin - Render Story List
+adminCtrl.renderStoryList = async(req, res) => {
+    try {
+        const responseStory = await resourcesServiceAPI.getAllStories();
+        console.log("---> adminCtrl.renderStoryList.getAllStories");
+        //console.log(responseStory.data);
+        const story = responseStory.data;
+        res.render("admin/story/index", { story });
+    } catch (err) {
+        console.error(err.message);
+        res.render("admin/story/index");
+    }
+};
+// AT-RESOURCES - Admin - Render Add Story Form
+adminCtrl.renderAddStoryForm = (req, res) => {
+    res.render("admin/sroty/add-story");
+};
+
+// AT-RESOURCES - Admin - Add Story
+adminCtrl.addStory = async(req, res) => {
+    const { title, description, status, category, img } = req.body;
+    let stories;
+    let request = {
+        title,
+        description,
+        category: category,
+        img,
+        status: parseInt(status),
+    };
+    await resourcesServiceAPI.addStory(request).then(result => {
+        console.log(result);
+        stories = result;
+    });
+
+    res.redirect("/admin/story");
+};
+
+// AT-RESOURCES - Admin - Render Edit Story Form
+adminCtrl.renderEditStoryForm = (req, res) => {
+    console.log("--> adminCtrl.renderEditStoryForm");
+    res.render("admin/story/edit-story");
+};
+
+// AT-RESOURCES - Admin - Update Story
+adminCtrl.updateStory = (req, res) => {
+    // Redirect
+    req.flash("success_msg", "Story Updated Successfully");
+    res.redirect("/admin/story");
+};
+
+// AT-RESOURCES - Admin - Delete Story
+adminCtrl.deleteStory = (req, res) => {
+    const errors = [];
+
+    // Redirect
+    req.flash("success_msg", "Story Deleted Successfully");
+    res.redirect("/admin/Story");
+};
 
 module.exports = adminCtrl;
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const path = require('path');
 
 // Admin Controller
-const { renderIndex } = require("../controllers/admin.controller");
+const { renderIndex,
+        renderStoryList,
+        renderAddStoryForm,
+        addStory,
+        renderEditStoryForm,
+        updateStory,
+        deleteStory
+    } = require("../controllers/admin.controller");
 
 // Helpers
 // const { isAdmin } = require("../helpers/auth");
@@ -13,4 +20,23 @@ const { renderIndex } = require("../controllers/admin.controller");
 // AT-RESOURCES - Admin - Index
 router.get("/", renderIndex);
 
+// AT-RESOURCES - Admin - Render Story List
+router.get("/story", renderStoryList);
+
+// AT-RESOURCES - Admin - Render Add Story
+router.get("/story/add", renderAddStoryForm);
+
+// AT-RESOURCES - Admin - Add Story
+router.post("/story/add", addStory);
+
+// AT-RESOURCES - Admin - Render Edit Story
+router.get("/story/edit/:id", renderEditStoryForm);
+
+// AT-RESOURCES - Admin - Update Story
+router.put("/story/edit/:id", updateStory);
+
+// AT-RESOURCES - Admin - Delete Story
+router.get("/story/delete/:id", deleteStory);
+
 module.exports = router;
+

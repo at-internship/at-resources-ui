b1ba8d0b01023c9cedee14a2c9e1d2f84cac9152
Antonio-Mendez <gibran.mendez@agilethought.com>
2020-11-05 13:15:33 -0600

    US-UI-RE-015 AT-RSC-UI Admin Module: Edit Story and creation of user story update method

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,13 +4,9 @@ const path = require('path');
 
 // Admin Controller
 const { renderIndex,
-        renderStoryList,
-        renderAddStoryForm,
-        addStory,
         renderEditStoryForm,
         updateStory,
-        deleteStory
-    } = require("../controllers/admin.controller");
+        deleteStory} = require("../controllers/admin.controller");
 
 // Helpers
 // const { isAdmin } = require("../helpers/auth");
@@ -20,23 +16,14 @@ const { renderIndex,
 // AT-RESOURCES - Admin - Index
 router.get("/", renderIndex);
 
-// AT-RESOURCES - Admin - Render Story List
-router.get("/story", renderStoryList);
-
-// AT-RESOURCES - Admin - Render Add Story
-router.get("/story/add", renderAddStoryForm);
-
-// AT-RESOURCES - Admin - Add Story
-router.post("/story/add", addStory);
-
-// AT-RESOURCES - Admin - Render Edit Story
+// AT-RESOURCES - Admin - Edit Story Form
 router.get("/story/edit/:id", renderEditStoryForm);
 
-// AT-RESOURCES - Admin - Update Story
-router.put("/story/edit/:id", updateStory);
+// ========== EDIT STORY =======
+
+router.put("/admin/story/edit-story/:id", updateStory);
 
 // AT-RESOURCES - Admin - Delete Story
-router.get("/story/delete/:id", deleteStory);
+router.get("/admin/story/delete/:id", deleteStory);
 
 module.exports = router;
-
